8a8bbe8bae67f05fac820fcc9174e8177d511881
Fujii Yosuke <fujiyou1121@gmail.com>
2017-02-20 10:16:46 +0900

    add odd art but not working three.js?

--- js-playground/p5-fez-jump/main.js
+++ js-playground/p5-fez-jump/main.js
@@ -1,17 +1,72 @@
-var renderer;
-var scene;
-var camera;
+var interval;
+var grid = 40;
+var posX;
+var posY;
+var w = 15;
+var h =15;
+var colors = {
+    1: '#ffffff',
+    2: '#e3ecda',
+    3: '#b9c7a5',
+    4: '#db0006',
+    5: '#a70007',
+    6: '#feb325',
+    7: '#01000a',
+    8: '#000000',
+    9: '#e27b68'
+  };
+var pixelArt = [
+0,0,0,6,4,4,0,0,0,0,0,0,0,
+0,0,6,5,4,4,0,0,0,0,0,0,0,
+0,0,0,5,4,4,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,3,2,1,1,1,1,1,1,1,1,1,0,
+3,2,2,1,1,1,1,1,1,1,1,1,1,
+3,2,1,7,1,1,1,1,1,1,1,7,1,
+3,2,1,1,1,1,8,8,8,8,1,1,1,
+3,2,1,1,1,1,9,9,8,8,1,1,1,
+3,2,2,1,1,1,1,1,8,1,1,1,1,
+0,3,3,2,1,1,1,1,1,1,1,1,0,
+0,0,0,0,3,3,2,1,2,0,0,0,0,
+0,3,1,1,2,1,1,1,1,3,3,0,0,
+0,0,3,1,1,1,1,1,1,3,0,0,0,
+0,0,0,3,1,1,1,1,1,0,0,0,0,
+0,0,0,3,2,1,1,1,1,0,0,0,0,
+0,0,0,3,2,1,1,1,1,0,0,0,0,
+0,0,0,3,2,3,3,2,3,0,0,0,0,
+0,0,3,2,0,0,2,3,0,0,0,0,0];
 
-var sphere, worldSphere;
-var texture;
-var canvas = document.getElementById('defaultCanvas0');
+function setup() {
+  createCanvas(450, 330);
+  strokeWeight(0);
 
-function init() {
-  scene = new THREE.Scene();
+  interval = setInterval(function () {
+    if (grid > 13) {
+      grid--;
+    } else {
+      stopInterval();
+    }
+  }, 10);
+}
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+function stopInterval() {
+  clearInterval(interval);
+}
 
-  camera.position.z = 15;
-  renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(0x270000, 1.0);
+function draw() {
+  background('#67cdfd')
+  posX = 50;
+  posY = 0;
+  for (var i=0; i<=pixelArt.length; i++) {
+    posX += w;
+    if (i % grid == 0) {
+      posX = 50;
+      posY += h;
+    }
+
+    if (pixelArt[i] > 0) {
+      fill(colors[pixelArt[i]]);
+      rect(posX, posY, w, h);
+    }
+  }
 }
--- js-playground/p5-three-art/main.js
+++ js-playground/p5-three-art/main.js
@@ -1,29 +1,150 @@
+var renderer;
+var scene;
+var camera;
+
+var sphere, worldSphere;
+var texture;
+var canvas = document.getElementById('defaultCanvas0');
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  camera.position.z = 15;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x270000, 1.0);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+
+  var directionalLightR = new THREE.DirectionalLight(0xff6633, 0.5);
+  directionalLightR.position.set(1, 0, .25);
+  scene.add(directionalLightR);
+
+  var directionalLightL = new THREE.DirectionalLight(0xff6633, 0.5);
+  directionalLightL.position.set(-1, 0, .25);
+  scene.add(directionalLightL);
+
+  var hemisphereLight = new THREE.HemisphereLight(0xD88D00, 0xD63600, 0.75);
+  scene.add(hemisphereLight);
+
+  document.body.appendChild(renderer.domElement);
+
+  var geometry = new THREE.SphereGeometry(3, 16, 16);
+  texture = new THREE.Texture(canvas);
+  var material = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    map: texture,
+    side: THREE.DoubleSide
+  });
+  sphere = new THREE.Mesh(geometry, material);
+  sphere.position.set(0, 0, 0);
+  sphere.rotation.y = -PI / 4 - 0.5;
+  scene.add(sphere);
+
+  var worldGeometry = new THREE.SphereGeometry(16, 12, 12);
+  var worldMaterial = new THREE.MeshPhongMaterial({
+    color: 0x863300,
+    map: texture,
+    side: THREE.DoubleSide
+  });
+  worldSphere = new THREE.Mesh(worldGeometry, worldMaterial);
+  worldSphere.position.set(0, 0, 0);
+  worldSphere.rotation.y = 2.5 * Math.PI;
+  scene.add(worldSphere);
+
+  render();
+}
+
+function render() {
+  camera.position.x = map(sin(frameCount * 0.01), -1, 1, 1.5, -1.5);
+  camera.position.z = map(cos(frameCount * 0.01), -1, 1, 15, 12);
+  texture.needsUpdate = true;
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.onload = init;
+window.addEventListener('resize', handleResize, false);
+
+var blobs = [];
+
+var limitLeft = 0, limitRight = 0, limitTop = 0, limitBottom = 0;
+var boxSize = 0;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(1024, 512, P2D);
+  boxSize = 600;
+  limitLeft = width / 2 - boxSize / 2;
+  limitRight = width / 2 + boxSize / 2;
+  limitTop = height / 2 - boxSize / 2;
+  limitBottom = height / 2 + boxSize / 2;
+
+  for (var i=0; i<100; i++) {
+    blobs[i] = new Blob(width / 2, height / 2, random(20, 75));
+  }
 }
 
 function draw() {
-  background(0);
+  background("#000000");
 
-  /* ライトの設定 */
-  var locY = (mouseY / height - 0.5) * (-2);
-  var locX = (mouseX / width - 0.5) * 2;
+  fill(255, 15);
+  rectMode(CENTER);
+  rect(width / 2, height / 2, boxSize, boxSize);
+  for (var i=0; i<blobs.length; i++) {
+    blobs[i].displayOutline();
+  }
+  for (var i=0; i<blobs.length; i++) {
+    blobs[i].display();
+    blobs[i].update();
+  }
+}
 
-  ambientLight(100, 80, 80);
-  pointLight(200, 200, 200, locX, locY, 0);
+function Blob(_x, _y, _sz) {
+  this.x = _x;
+  this.y = _y;
+  this.sz = _sz;
 
-  rotateY(frameCount * 0.0001);
-  orbitControl();
+  this.vx = random(-1.5, 1.5);
+  this.vy = random(-1.5, 1.5);
 
-  for (var j=0; j<10; j++) {
-    push();
-    for (var i=0; i<100; i++) {
-      translate(sin(frameCount * 0.01 + j) * 200, sin(frameCount * 0.001 + j) * 300, i * 0.1);
-      rotateZ(frameCount * 0.002);
-      push();
-      sphere(2, 10, 100);
-      pop();
+  this.display = function () {
+    noStroke();
+    fill(0);
+    ellipse(this.x, this.y, this.sz, this.sz);
+  };
+
+  this.displayOutline = function () {
+    noStroke();
+    fill(255);
+    ellipse(this.x, this.y, this.sz+10, this.sz+10);
+  };
+
+  this.update = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x > limitRight - this.sz / 2) {
+      this.x = limitRight - this.sz / 2;
+      this.vx *= -1.0;
     }
-    pop();
+    if (this.x < limitLeft + this.sz / 2) {
+      this.x = limitLeft + this.sz / 2;
+      this.vx *= -1.0;
+    }
+    if (this.y > limitBottom - this.sz / 2) {
+      this.y = limitBottom - this.sz / 2;
+      this.vy *= -1.0;
+    }
+    if (this.y < limitTop + this.sz / 2) {
+      this.y = limitTop + this.sz / 2;
+      this.vy *= -1.0;
+    }
   }
 }
