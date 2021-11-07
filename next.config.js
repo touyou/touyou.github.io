4015204b4f7325a82ad1c5916d56316bc04206ad
touyou <fujiyou1121@gmail.com>
2021-11-07 18:25:22 +0900

    :+1: setup some dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const withLinaria = require('next-linaria')
+
+const repoName = '/nextjs-linaria'
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withLinaria({
   reactStrictMode: true,
-}
+  linaria: {
+    /* linaria options here */
+  }
+})
