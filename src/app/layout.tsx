e481684824dd9f8c99cb1c2cb4a5b2506ca15657
touyou <fujiyou1121@gmail.com>
2023-12-10 13:34:57 +0900

    app routerに最適化していく

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import { randomBytes } from 'crypto'
-import './globals.css'
+import "./globals.css";
 
-import NextHead from 'next/head'
+import { Metadata, Viewport } from "next";
+import Script from "next/script";
 
-const siteName = 'touyou.dev'
-const description = 'touyou (Fujii Yosuke) / Japanese Software Engineer'
-const siteUrl = 'https://touyou.dev'
+const siteName = "touyou.dev";
+const description = "touyou (Fujii Yosuke) / Japanese Software Engineer";
+const siteUrl = "https://touyou.dev";
 
-export const metadata = {
+export const metadata: Metadata = {
+  authors: [{ name: "touyou" }],
+  keywords: ["touyou", "touyoubuntu", "とうよう", "とーよー"],
   title: siteName,
   description,
   openGraph: {
@@ -15,47 +17,65 @@ export const metadata = {
     description,
     url: siteUrl,
     siteName,
-    locale: 'ja_JP',
-    type: 'website',
+    locale: "ja_JP",
+    type: "website",
+    images: [
+      {
+        url: "/ogp.jpg",
+        width: 1200,
+        height: 628,
+      },
+      {
+        url: "/ogp_facebook.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: siteName,
     description,
-  }
-}
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  manifest: "favicon/site.webmanifest",
+};
+
+export const viewport: Viewport = {
+  themeColor: "white",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 const GA_TRACKING_ID = "UA-136500666-1";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const nonce = randomBytes(128).toString("base64");
-  const csp = `object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http: 'nonce-${nonce}' 'strict-dynamic'`;
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='ja'>
-      <NextHead>
-        <meta httpEquiv="Content-Security-Policy" content={csp} />
-        <script
-          async
-          nonce={nonce}
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `
+    <html lang="ja">
+      <body>{children}</body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
             `,
-          }}
-        />
-      </NextHead>
-      <body>
-        {children}
-      </body>
+        }}
+      />
     </html>
-  )
+  );
 }
